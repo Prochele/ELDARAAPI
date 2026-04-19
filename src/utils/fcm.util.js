e0d70797eeefd090bdef8a72ea49d53fd1bd3b2c
Prochele Software Solutions Pvt Ltd <development@prochele.com>
2026-04-19 13:02:05 +0530

    fix start path for railway

--- src/utils/fcm.util.js
+++ src/utils/fcm.util.js
@@ -1,6 +1,7 @@
 const admin = require('firebase-admin');
 
-const serviceAccount = require('../config/firebase.json');
+//const serviceAccount = require('../config/firebase.json');
+const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
