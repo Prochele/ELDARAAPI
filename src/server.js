7d6851132ba4746909deb2a21642afbaca3626f6
Prochele Software Solutions Pvt Ltd <development@prochele.com>
2026-04-19 12:29:28 +0530

    fix start path for railway

--- src/server.js
+++ src/server.js
@@ -5,14 +5,35 @@
 // app.listen(PORT, () => {
 //   console.log(`🚀 ELDARA API running on port ${PORT}`);
 // });
-const app = require('./app');
 
+
+// const app = require('./app');
+
+// const PORT = process.env.PORT || 3000;
+
+// const { startScheduler } = require('./services/scheduler.service');
+
+// startScheduler();
+
+// app.listen(PORT, '0.0.0.0', () => {
+//   console.log(`🚀 ELDARA API running on port ${PORT}`);
+// });
+
+const app = require('./app');
 const PORT = process.env.PORT || 3000;
 
 const { startScheduler } = require('./services/scheduler.service');
 
-startScheduler();
+(async () => {
+  try {
+    // start background jobs
+    startScheduler();
 
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`🚀 ELDARA API running on port ${PORT}`);
-});
+    app.listen(PORT, '0.0.0.0', () => {
+      console.log(`🚀 ELDARA API running on port ${PORT}`);
+    });
+
+  } catch (err) {
+    console.error('Startup error:', err);
+  }
+})();
