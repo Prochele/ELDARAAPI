29898e327b9a41b79235634c5bfd0e4a105acd33
Prochele Software Solutions Pvt Ltd <development@prochele.com>
2026-04-19 11:51:45 +0530

    use railway internal db url

--- src/config/db.js
+++ src/config/db.js
@@ -1,19 +1,19 @@
 const mysql = require('mysql2/promise');
 const env = require('./env');
 
-const pool = mysql.createPool({
-  host: env.DB_HOST,
-  user: env.DB_USER,
-  password: env.DB_PASSWORD,
-  database: env.DB_NAME,
-  port: env.DB_PORT,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-  ssl: {
-    rejectUnauthorized: false
-  }
+// const pool = mysql.createPool({
+//   host: env.DB_HOST,
+//   user: env.DB_USER,
+//   password: env.DB_PASSWORD,
+//   database: env.DB_NAME,
+//   port: env.DB_PORT,
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+//   ssl: {
+//     rejectUnauthorized: false
+//   }
 
-});
-
+// });
+const pool = mysql.createPool(process.env.DATABASE_URL);
 module.exports = pool;
