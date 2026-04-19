77fe88bf7a1dd49d661f1e228d286e3b5532fd42
Prochele Software Solutions Pvt Ltd <development@prochele.com>
2026-04-19 12:51:45 +0530

    fix start path for railway

--- src/app.js
+++ src/app.js
@@ -1,23 +1,38 @@
 
-
+console.log("STEP 1");
 const express = require('express');
-
+console.log("STEP 2");
 const authRoutes = require('./routes/auth.routes');
+console.log("STEP 3");
 const dailyActivityRoutes = require('./routes/dailyActivity.routes');
+console.log("STEP 4");
 const geoRoutes = require('./routes/geo.routes');   // ✅ ADD THIS
+console.log("STEP 5");
 const otpRoutes = require('./routes/otp.routes');
+console.log("STEP 6");
 const errorHandler = require('./middlewares/error.middleware');
+console.log("STEP 7");
 const signupRoutes = require('./routes/signup.routes');
+console.log("STEP 8");
 const patronRoutes = require('./routes/patron.routes');
+console.log("STEP 9");
 const fileRoutes = require('./routes/file.routes');
+console.log("STEP 10");
 const doctorAppointmentRoutes = require('./routes/doctorAppointment.routes');
+console.log("STEP 11");
 const ocrRoutes = require('./routes/ocr.routes');
+console.log("STEP 12");
 const masterRoutes = require('./routes/master.routes');
+console.log("STEP 13");
 const medicineRoutes = require('./routes/medicine.routes');
+console.log("STEP 14");
 const deviceRoutes = require('./routes/device.routes');
+console.log("STEP 15");
 const app = express();
 const medicineScheduleRoutes = require('./routes/medicineSchedule.routes');
+console.log("STEP 16");
 const vitalsRoutes = require('./routes/vitals.routes');
+console.log("STEP 17");
 
 /**
  * Global middlewares
